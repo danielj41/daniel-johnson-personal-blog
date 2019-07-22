7aae943d09df00b5ace71278a202f5320d80c1b1
danielj41 <danielj41@gmail.com>
2019-07-21 17:23:00 -0700

    Make typography theme compatible with dark mode

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,14 +1,34 @@
 import Typography from "typography"
 import parnassusTheme from "typography-theme-parnassus"
+import { MOBILE_MEDIA_QUERY } from "typography-breakpoint-constants"
 
-parnassusTheme.overrideThemeStyles = ({ rhythm }, options) => ({
+const customizedParnassusTheme = {
+  ...parnassusTheme,
+  bodyColor: "var(--body-color)",
+  headerColor: "var(--header-color)",
+}
+
+customizedParnassusTheme.overrideThemeStyles = ({ rhythm }, options) => ({
   a: {
+    color: "var(--link-color)",
     boxShadow: "none",
     textDecoration: "underline",
   },
+  "a:hover, a:active": {
+    textDecoration: "none",
+  },
+  blockquote: {
+    color: "var(--blockquote-color)",
+    borderColor: "var(--blockquote-border-color)",
+  },
+  [MOBILE_MEDIA_QUERY]: {
+    blockquote: {
+      borderColor: "var(--blockquote-border-color)",
+    },
+  },
 })
 
-const typography = new Typography(parnassusTheme)
+const typography = new Typography(customizedParnassusTheme)
 
 // Export helper functions
 export const { scale, rhythm, options } = typography
