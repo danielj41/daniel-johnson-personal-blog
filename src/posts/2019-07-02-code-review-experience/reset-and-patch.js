c93cd7b2d512965b56928a82ddde8d7464cd7268
danielj41 <danielj41@gmail.com>
2019-08-04 22:42:37 -0700

    Improve alt text

--- src/posts/2019-07-02-code-review-experience/reset-and-patch.js
+++ src/posts/2019-07-02-code-review-experience/reset-and-patch.js
@@ -6,7 +6,7 @@ const ResetAndPatch = () => (
   <TerminalCodeBlock
     language="diff"
     prompt="daniel@macbook $"
-    title="📁 ~/example — bash — Terminal"
+    title="📁 ~/example — git status — Terminal"
     code={`daniel@macbook $ git fetch
  
 daniel@macbook $ git rebase origin/master
@@ -33,30 +33,6 @@ Changes not staged for commit:
 -	modified:   src/utils.js
  
 no changes added to commit (use "git add" and/or "git commit -a")
- 
-daniel@macbook $ git diff
- 
-diff --git a/src/utils.js b/src/utils.js
-index 91867c3..892aa17 100644
---- a/src/utils.js
-+++ b/src/utils.js
-@@ -2,11 +2,21 @@ function increment(x) {
-   return x + 1;
- }
-
-+function incrementByAmount(x, amount) {
-+  return x + amount;
-+}
-+
- function decrement(x) {
-   return x - 1;
- }
-
-+function decrementByAmount(x, amount) {
-+  return x - amount;
-+}
-+
-:
 `}
   />
 );
