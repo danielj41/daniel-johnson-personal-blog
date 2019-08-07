7f92c888381ee9e98afe0416c6a12c64c31e9cd6
danielj41 <danielj41@gmail.com>
2019-08-06 21:34:29 -0700

    code stylistic changes
    I have opinions, ok?

--- src/components/layout/seo.js
+++ src/components/layout/seo.js
@@ -30,45 +30,45 @@ function SEO({ description, lang = "en", meta = [], title = "", image }) {
       defaultTitle={site.siteMetadata.title}
       meta={[
         {
-          name: `description`,
+          name: "description",
           content: metaDescription,
         },
         {
-          property: `og:title`,
+          property: "og:title",
           content: title || site.siteMetadata.title,
         },
         {
-          property: `og:description`,
+          property: "og:description",
           content: metaDescription,
         },
         {
-          property: `og:type`,
-          content: `website`,
+          property: "og:type",
+          content: "website",
         },
         {
-          name: `twitter:card`,
-          content: `summary`,
+          name: "twitter:card",
+          content: "summary",
         },
         {
-          name: `twitter:creator`,
+          name: "twitter:creator",
           content: site.siteMetadata.author,
         },
         {
-          name: `twitter:title`,
+          name: "twitter:title",
           content: title || site.siteMetadata.title,
         },
         {
-          name: `twitter:description`,
+          name: "twitter:description",
           content: metaDescription,
         },
         ...(image
           ? [
               {
-                property: `og:image`,
+                property: "og:image",
                 content: `${site.siteMetadata.rootUrl}${image}`,
               },
               {
-                property: `twitter:image`,
+                property: "twitter:image",
                 content: `${site.siteMetadata.rootUrl}${image}`,
               },
             ]
